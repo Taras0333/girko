4e0397165b6446d1b6399c33e88e122fc77b18a9
Taras Khomyn <taraskhomyn@MacBook-Air-Taras.local>
2020-12-10 17:46:48 +0200

    2

--- src/infoBar/InfoBar.js
+++ src/infoBar/InfoBar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './infoBar.scss';
 import Profile from '../assets/profile-ava.jpg';
+import facebook from '../assets/icons/facebook.svg';
+import globe from '../assets/icons/globe.svg';
+import instagram from '../assets/icons/instagram.svg';
 
 export default function InfoView() {
     return (
@@ -13,6 +16,31 @@ export default function InfoView() {
                             <img className='profile-photo' src={Profile} alt='profile photo'/>
                         </div>
                     </div>
+                    <div className='social-icons-wrap'>
+                        <a className='social-link globe' href="http://www.nakonechnyi.com/" target='_blank'>
+                            <img src={globe} className='social-icon' alt='globe icon'/>
+                        </a>
+                        <a className='social-link facebook' href="https://www.facebook.com/nakonechnyi.o" target='_blank'>
+                            <img src={facebook} className='social-icon'alt='facebook icon'/>
+                        </a>
+                        <a className='social-link instagram' href="https://www.instagram.com/nakonechnyi_o/" target='_blank'>
+                            <img src={instagram} className='social-icon' alt='instagram icon'/>
+                        </a>
+                    </div>
+                </div>
+                <div className='right-side'>
+                    <div className='right-top'>
+                        <div className='user-name-wrap'>
+                            <span className='user-name'>Олександр Наконечний</span>
+                            <div className='status-cont'>
+                                <span className='status'>PRO</span>
+                            </div>
+                        </div>
+                        <div className='rewiev-wrap'>
+                            <span className='rewiev'>Залишити відгук</span>
+                        </div>
+                    </div>
+                    <div className='right-bottom'></div>
                 </div>
              </div>
         </div>
